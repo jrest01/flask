4b03cd4be37181fce741b767c540651b97524112
jrest01 <julian11072000@hotmail.com>
2023-07-17 21:30:42 -0500

    Features to request and response

--- README.md
+++ README.md
@@ -28,5 +28,5 @@ OR
 py main.py
 
 # Navigate to:
-http://127.0.0.1:5000/flask
+http://127.0.0.1:5000/
 ```
--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect
 
 
 
 app = Flask(__name__)
 
 
-@app.route('/flask')
+@app.route('/')
+def index():
+    """
+        Gets the user IP and save it on a cookie and redirects the user to 'hello'
+    """
+    user_ip = request.remote_addr
+    
+    response = make_response(redirect('hello'))
+    response.set_cookie('user_ip',user_ip)
+
+    return response
+
+
+@app.route('/hello')
 def hello():
-    return "Platzi, hellow from Flask"
+    """
+        Gets the cookie user_ip's value
+    """
+    user_ip = request.cookies.get('user_ip')
+    return f"Hello, for IP address is {user_ip}"
