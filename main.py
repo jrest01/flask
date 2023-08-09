bc108f3c9f1d3998ddb4c1108ae8a8d1736e8cc6
jrest01 <julian11072000@hotmail.com>
2023-08-08 20:05:00 -0500

    Basic project complete

--- main.py
+++ main.py
@@ -1,9 +1,30 @@
-from flask import Flask, request, make_response, redirect, render_template
+from flask import Flask, request, make_response, redirect, render_template, session, url_for, flash
+from flask_bootstrap import Bootstrap4
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
+import unittest
+
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap4(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRET'
 
 todos = ['Estudiar', 'Trabajar', 'Ir al gimnasio']
 
 
-app = Flask(__name__)
+class LoginForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+@app.cli.command()
+def test():
+    tests = unittest.TestLoader().discover('tests')
+    unittest.TextTestRunner().run(tests)
 
 
 @app.errorhandler(404)
@@ -24,17 +45,34 @@ def index():
     user_ip = request.remote_addr
     
     response = make_response(redirect('hello'))
-    response.set_cookie('user_ip',user_ip)
+    session['user_ip'] = user_ip
 
     return response
 
 
-@app.route('/hello')
+@app.route('/hello', methods=['GET', 'POST'])
 def hello():
     """
         Gets the cookie user_ip's value and render the template 'hello.html'
     """
-    user_ip = request.cookies.get('user_ip')
+    user_ip = session.get('user_ip')
+    login_form = LoginForm()
+    username = session.get('username')
+
     context = {'user_ip' : user_ip, 
-               'todos' : todos}
+               'todos' : todos,
+               'login_form' : login_form,
+               'username' : username           
+    }
+
+    if request.method == 'POST':
+        
+
+        username = login_form.username.data
+        session['username'] = username
+        flash('Username succesfull registred')
+        return redirect(url_for('index'))
+
     return render_template('hello.html', **context)
+
+
