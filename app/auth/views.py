746cf3cc6dfdaf2f23d2bb17f432d9e805cb2bad
jrest01 <julian11072000@hotmail.com>
2023-08-17 21:41:58 -0500

    Connect to database and full login

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,6 +1,11 @@
 from . import auth
 from app.forms import LoginForm
-from flask import render_template, request, session, url_for, flash, redirect
+from flask import render_template, request, url_for, flash, redirect
+from app.firestore_service import get_user, user_put
+from app.models import UserData, UserModel
+from flask_login import login_user, login_required, logout_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
+
 
 @auth.route('/login', methods = ['GET', 'POST'])
 def login():
@@ -9,9 +14,71 @@ def login():
         'login_form' : login_form
     }
 
-    if request.method == 'POST':   
-        username = login_form.username.data
-        session['username'] = username
-        flash('Username succesfull registred')
+    if current_user.is_authenticated:
+        flash('Already logged')
         return redirect(url_for('index'))
-    return render_template('login.html', **context)
+
+    else:
+        if request.method == 'POST':   
+            username = login_form.username.data
+            password = login_form.password.data
+
+            user_doc = get_user(username)
+
+            if user_doc.to_dict() is not None:
+                password_from_db = user_doc.to_dict()['password']
+                if check_password_hash(password_from_db, password):
+                    user_data = UserData(username, password)
+                    user = UserModel(user_data)
+
+                    login_user(user)
+
+                    flash('Welcome back!')
+
+                    redirect(url_for('hello'))
+                else:
+                    flash('Incorrect Password')
+            else:
+                flash('User not found :( )')
+
+            return redirect(url_for('index'))
+        return render_template('login.html', **context)
+    
+
+@auth.route('signup', methods=['GET', 'POST'])
+def signup():
+    signup_form = LoginForm()
+    context = {
+        'signup_form' : signup_form
+    }
+
+    if request.method == 'POST':   
+        username = signup_form.username.data
+        password = signup_form.password.data
+        user_doc = get_user(username)
+        
+        if user_doc.to_dict() is None:
+            password_hash = generate_password_hash(password)
+            user_data = UserData(username, password_hash)
+            user_put(user_data)
+            user = UserModel(user_data)
+
+            login_user(user)
+
+            flash('Welcome!')
+            return redirect(url_for('hello'))
+        
+        else:
+            flash('User already exists')
+        
+    return render_template('signup.html', **context)
+
+
+
+@auth.route('logout')
+@login_required
+def logout():
+    logout_user()
+    flash('Come back soon')
+
+    return redirect(url_for('auth.login'))
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import  request, make_response, redirect, render_template, session, url_for, flash
 import unittest
-
+from flask import  request, make_response, redirect, render_template, session, url_for, flash
+from flask_login import login_required, current_user
 from app import create_app
 from app.forms import LoginForm
 
@@ -40,23 +40,19 @@ def index():
 
 
 @app.route('/hello', methods=['GET'])
+@login_required
 def hello():
     """
         Gets the cookie user_ip's value and render the template 'hello.html'
     """
     user_ip = session.get('user_ip')
-    username = session.get('username')
+    username = current_user.id
 
     context = {'user_ip' : user_ip, 
                'todos' : get_todos(username),
                'username' : username           
     }
 
-    users = get_users()
-
-    for user in users:
-        print(user.id)
-        print(user.to_dict()['password'])
 
     return render_template('hello.html', **context)
 
