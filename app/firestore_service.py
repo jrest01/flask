4106b36f1bbb7dd08e29b169708768bc14df0ca0
jrest01 <julian11072000@hotmail.com>
2023-09-07 20:41:21 -0500

    Adding bootstrap

--- app/firestore_service.py
+++ app/firestore_service.py
@@ -29,18 +29,32 @@ def get_todos(user_id):
     return db.collection('users').document(user_id).collection('todos').get()
 
 
+def get_todo(user_id, todo_id):
+    todo_ref = _get_todo_ref(user_id, todo_id)
+    # return todo_ref.get()
+    return db.collection('users').document(user_id).collection('todos').document(todo_id).get()
+
+
 def todo_put(user_id, todo_description):
     todos_ref = db.collection('users').document(user_id).collection('todos')
     todos_ref.add({'description' : todo_description, 'done' : False})
 
+
 def todo_delete(user_id, todo_id):
     todo_ref = _get_todo_ref(user_id, todo_id)
     todo_ref.delete()
+    
 
 def todo_update(user_id, todo_id, done):
     todo_done = not bool(done)
     todo_ref = _get_todo_ref(user_id, todo_id)
     todo_ref.update({'done': todo_done})
     
+
+def todo_update_description(user_id, todo_id, description):
+    todo_ref = _get_todo_ref(user_id, todo_id)
+    todo_ref.update({'description': description})
+    
+
 def _get_todo_ref(user_id, todo_id):
     return db.collection('users').document(user_id).collection('todos').document(todo_id)
--- app/forms.py
+++ app/forms.py
@@ -2,6 +2,12 @@ from flask_wtf import FlaskForm
 from wtforms.fields import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired
 
+text_styles = {
+'class': 'text-uppercase'
+}
+
+
+
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
@@ -14,8 +20,18 @@ class TodoForm(FlaskForm):
 
 
 class TodoDeleteForm(FlaskForm):
-    delete = SubmitField('Delete')
+    
+    delete = SubmitField('Delete', render_kw={'class': 'btn btn-danger'})
+
+
+class TodoStatusUpdate(FlaskForm):
+    update = SubmitField('Done/ToDo', render_kw={'class': 'btn btn'})
+    
+
+class TodoUpdateButton(FlaskForm):
+    update = SubmitField('Update', render_kw={'class': 'btn btn-warning'})
 
 
 class TodoUpdateForm(FlaskForm):
-    update = SubmitField('update')
+    description = StringField('New Description', validators=[DataRequired()])
+    submit = SubmitField('Submit')
